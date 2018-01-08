3fab770dd1f50ffbc0a4a8d43def85f96a8f1f04
Alessandro <alessandro.gaboardi@mindtek.it>
2018-01-08 15:58:29 +0100

    Adapters for write section

--- app/src/main/java/it/mindtek/ruah/adapters/holders/EmptyLetterHolder.kt
+++ app/src/main/java/it/mindtek/ruah/adapters/holders/EmptyLetterHolder.kt
@@ -1,7 +1,10 @@
 package it.mindtek.ruah.adapters.holders
 
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
 /**
  * Created by alessandrogaboardi on 08/01/2018.
  */
-class EmptyLetterHolder {
+class EmptyLetterHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 }
--- app/src/main/java/it/mindtek/ruah/adapters/holders/LettersHolder.kt
+++ app/src/main/java/it/mindtek/ruah/adapters/holders/LettersHolder.kt
@@ -1,7 +1,13 @@
 package it.mindtek.ruah.adapters.holders
 
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import kotlinx.android.synthetic.main.item_letter_selected.view.*
+
 /**
  * Created by alessandrogaboardi on 08/01/2018.
  */
-class LettersHolder {
+class LettersHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val letter = itemView.letter
+    val card = itemView.card
 }
