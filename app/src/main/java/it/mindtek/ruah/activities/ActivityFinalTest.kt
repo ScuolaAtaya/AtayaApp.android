775c9be8eb1103f67b606e6d6910f84e89e3c103
Andrea Magoni <andrea.magoni@mindtek.it>
2020-06-17 18:09:37 +0200

    Updated ActivityFinalTest

--- app/src/main/java/it/mindtek/ruah/activities/ActivityFinalTest.kt
+++ app/src/main/java/it/mindtek/ruah/activities/ActivityFinalTest.kt
@@ -1,12 +1,55 @@
 package it.mindtek.ruah.activities
 
+import android.annotation.TargetApi
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
+import it.mindtek.ruah.R
+import it.mindtek.ruah.enums.Category
+import it.mindtek.ruah.kotlin.extensions.compat21
+import it.mindtek.ruah.kotlin.extensions.db
 
 class ActivityFinalTest : AppCompatActivity() {
+    var unitId: Int = -1
+    var category: Category? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_final_test)
+        unitId = intent.getIntExtra(ActivityUnit.EXTRA_UNIT_ID, -1)
+        category = Category.from(intent.getIntExtra(ActivityIntro.EXTRA_CATEGORY_ID, -1))
+        setup()
+        // val fragment = FragmentUnderstandVideo.newInstance(unitId)
+        // replaceFragment(fragment, R.id.placeholder, false)
+    }
+
+    private fun setup() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(category!!.title)
+        val unitObservable = db.unitDao().getUnitByIdAsync(unitId)
+        unitObservable.observe(this, Observer { unit ->
+            unit?.let {
+                val color = ContextCompat.getColor(this, unit.color)
+                val colorDark = ContextCompat.getColor(this, unit.colorDark)
+                supportActionBar?.setBackgroundDrawable(ColorDrawable(color))
+                compat21(@TargetApi(21) {
+                    val window = window
+                    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                    window.statusBarColor = colorDark
+                }, {})
+            }
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return false
     }
 }
