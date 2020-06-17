b5e330e241bd143fd2d8d7a7c9486394482ea078
Andrea Magoni <andrea.magoni@mindtek.it>
2020-06-17 17:29:06 +0200

    Added ActivityFinalTest

---
+++ app/src/main/java/it/mindtek/ruah/activities/ActivityFinalTest.kt
@@ -0,0 +1,12 @@
+package it.mindtek.ruah.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityFinalTest : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_final_test)
+    }
+}
