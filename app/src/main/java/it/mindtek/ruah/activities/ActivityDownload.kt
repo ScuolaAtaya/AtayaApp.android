9d9315e1fd27e1ab142eb06fcbe28072f2c20c6b
Andrea Magoni <andrea.magoni@mindtek.it>
2020-06-19 09:48:06 +0200

    Clean up code

--- app/src/main/java/it/mindtek/ruah/activities/ActivityDownload.kt
+++ app/src/main/java/it/mindtek/ruah/activities/ActivityDownload.kt
@@ -1,17 +1,12 @@
 package it.mindtek.ruah.activities
 
-import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.content.pm.PackageManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import it.mindtek.ruah.R
 import it.mindtek.ruah.pojos.Download
 import it.mindtek.ruah.services.DownloadService
@@ -19,23 +14,11 @@ import kotlinx.android.synthetic.main.activity_download.*
 
 class ActivityDownload : AppCompatActivity() {
 
-    private val PERMISSION_REQUEST_CODE = 1337
-    private val DOWNLOAD = "download"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_download)
-
         registerReceiver()
-        downloadFile()
-    }
-
-    private fun downloadFile() {
-//        if (checkPermission()) {
-            startDownload()
-//        } else {
-//            requestPermission()
-//        }
+        startDownload()
     }
 
     private fun startDownload() {
@@ -54,7 +37,7 @@ class ActivityDownload : AppCompatActivity() {
     private val broadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             if (intent.action == MESSAGE_PROGRESS) {
-                val download = intent.getParcelableExtra<Download>(DOWNLOAD)
+                val download = intent.getParcelableExtra<Download>(DOWNLOAD)!!
                 progress.progress = download.progress
                 if (download.progress == 100) {
                     progressText.text = getString(R.string.downloaded)
@@ -65,35 +48,15 @@ class ActivityDownload : AppCompatActivity() {
             } else if (intent.action == PARSE_COMPLETED) {
                 progress.isIndeterminate = false
                 progress.progress = 100
-                val intent = Intent(this@ActivityDownload, ActivityUnits::class.java)
-                startActivity(intent)
+                startActivity(Intent(this@ActivityDownload, ActivityUnits::class.java))
                 finish()
             }
         }
     }
 
-    private fun checkPermission(): Boolean {
-        val result = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        return result == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(this@ActivityDownload, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            PERMISSION_REQUEST_CODE -> if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startDownload()
-            } else {
-                Snackbar.make(constraint, getString(R.string.download_permission_denied), Snackbar.LENGTH_LONG).show()
-            }
-        }
-    }
-
     companion object {
-        val MESSAGE_PROGRESS = "message_progress"
-        val PARSE_COMPLETED = "book_parse_completed"
+        const val MESSAGE_PROGRESS = "message_progress"
+        const val PARSE_COMPLETED = "book_parse_completed"
+        const val DOWNLOAD = "download"
     }
 }
