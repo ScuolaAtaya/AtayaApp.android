ffe0cd2534c03f962a8cca9d671c916aa97a9860
Andrea Magoni <andrea.magoni@mindtek.it>
2020-06-29 17:48:26 +0200

    Added ActivityUnderstandQuestion

---
+++ app/src/main/java/it/mindtek/ruah/activities/ActivityUnderstandQuestion.kt
@@ -0,0 +1,11 @@
+package it.mindtek.ruah.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityUnderstandQuestion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_understand_question)
+    }
+}
