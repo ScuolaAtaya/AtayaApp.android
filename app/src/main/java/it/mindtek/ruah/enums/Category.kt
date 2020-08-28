039fd8164708496640b3c08c34fe53038caac8b9
Andrea Magoni <andrea.magoni@mindtek.it>
2020-08-28 09:44:21 +0200

    Updated category

--- app/src/main/java/it/mindtek/ruah/enums/Category.kt
+++ app/src/main/java/it/mindtek/ruah/enums/Category.kt
@@ -12,8 +12,9 @@ enum class Category(val value: Int, @StringRes val title: Int, @DrawableRes val 
     UNDERSTAND(0, R.string.section_understand, R.drawable.volume, R.string.section_understand_description, R.raw.capiamo),
     TALK(1, R.string.section_talk, R.drawable.mic, R.string.section_talk_description, R.raw.parliamo),
     READ(2, R.string.section_read, R.drawable.eye, R.string.section_read_description, R.raw.leggiamo),
-    WRITE(3, R.string.section_write, R.drawable.edit, R.string.section_write_description, R.raw.scriviamo);
-    // TODO add FINAL_TEST
+    WRITE(3, R.string.section_write, R.drawable.edit, R.string.section_write_description, R.raw.scriviamo),
+    // TODO add description and audio
+    FINAL_TEST(4, R.string.section_final_test, R.drawable.edit, R.string.section_final_test, R.raw.scriviamo);
 
     companion object {
         fun from(value: Int): Category? = values().firstOrNull { it.value == value }
