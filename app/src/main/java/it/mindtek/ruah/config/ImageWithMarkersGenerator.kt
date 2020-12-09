dd6b03a3d41691d9f98bf1e4ae2fc0b98548cb31
Andrea Magoni <andrea.magoni@mindtek.it>
2020-12-09 15:03:42 +0100

    Fixed markers size

--- app/src/main/java/it/mindtek/ruah/config/ImageWithMarkersGenerator.kt
+++ app/src/main/java/it/mindtek/ruah/config/ImageWithMarkersGenerator.kt
@@ -3,11 +3,13 @@ package it.mindtek.ruah.config
 import android.content.Context
 import android.graphics.*
 import android.text.TextPaint
+import android.util.DisplayMetrics
 import androidx.core.content.ContextCompat
 import it.mindtek.ruah.R
 import it.mindtek.ruah.db.models.ModelMarker
 import org.jetbrains.anko.dip
 import org.jetbrains.anko.sp
+import org.jetbrains.anko.windowManager
 import java.io.File
 
 
@@ -24,14 +26,16 @@ object ImageWithMarkersGenerator {
         options.inMutable = true
         val bitmap = BitmapFactory.decodeFile(file.absolutePath, options)
         val canvas = Canvas(bitmap)
+        val displayMetrics = DisplayMetrics()
+        context.windowManager.defaultDisplay.getMetrics(displayMetrics)
         markerList.forEach {
-            createMarker(it, canvas)
+            createMarker(it, canvas, displayMetrics.widthPixels)
         }
         return bitmap
     }
 
-    private fun createMarker(marker: ModelMarker, canvas: Canvas) {
-        val radius = context.dip(12).toFloat()
+    private fun createMarker(marker: ModelMarker, canvas: Canvas, width: Int) {
+        val radius = (context.dip(14) * canvas.width / width).toFloat()
         var x = (marker.x * canvas.width).toFloat()
         var y = (marker.y * canvas.height).toFloat()
         x = when {
@@ -50,7 +54,7 @@ object ImageWithMarkersGenerator {
         canvas.drawCircle(x, y, radius, paint)
         val paintText = TextPaint()
         paintText.color = ContextCompat.getColor(context, R.color.white)
-        paintText.textSize = context.sp(16).toFloat()
+        paintText.textSize = (context.sp(18) * canvas.width / width).toFloat()
         paintText.textAlign = Paint.Align.CENTER
         val textOffset: Float = (paintText.descent() - paintText.ascent()) / 2 - paintText.descent()
         canvas.drawText(marker.id, x, y + textOffset, paintText)
